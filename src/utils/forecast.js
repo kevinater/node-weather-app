53850a9fc5dd5fe1c7eebe614a6f7dcf3e7731bc
Kevin Vahdat <kevinvahdat35@gmail.com>
2020-07-28 20:34:19 -0400

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=e3bfafc25fcc0eb743dea387a6216172&query=' + latitude + ',' + longitude + '&units=f'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, "Current temperature is " + body.current.temperature + " and it feels like " + body.current.feelslike);
+        }
+    })
+}
+
+module.exports = forecast
